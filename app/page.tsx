7f81a6645b2208890ac0ed7ba65a062b4c4afb49
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-02-27 12:15:40 -0300

    start of main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex flex-grow flex-col items-center justify-center bg-blue-500">
       <div>
         <h1 className="text-center">oi</h1>
         <Button>Me clica</Button>
---
+++ components/PageHeader.tsx
@@ -0,0 +1,10 @@
+export default function PageHeader() {
+
+    return (
+        <main>
+            <div className="h-1/5 bg-red-500 min-w-screen">
+                <h1>oi</h1>
+            </div>
+        </main>
+    )
+}
