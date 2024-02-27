fe150ee49e4b2050cc6d146a079b64c658291cd1
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-02-27 20:31:18 -0300

    feature: finish of header

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -1,22 +1,37 @@
 import Image from 'next/image';
 import Icon from '../public/icon.png';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 export default function PageHeader() {
   return (
-    <main className="w-full" style={{ height: '10%', backgroundColor: 'red' }}>
-      <div className="flex w-full h-full bg-slate-300">
-        <div className="flex w-96 m-w-full bg-red-300">
-          <div className="flex min-w-48 bg-blue-200 h-full justify-center items-center">
-            <div className="flex w-2/6 bg-blue-300 h-2/3 items-center justify-center rounded-md">
+    <main className="w-full" style={{ height: '10%' }}>
+      <div className="flex w-full h-full justify-between">
+        <div className="flex w-full">
+          {/* container*/}
+          <div className="flex min-w-48 h-full justify-center items-center">
+            <div className="flex w-2/6 h-2/3 items-center justify-center rounded-md">
               <Image src={Icon} width={30} height={30} alt="paquimetro" />
             </div>
-            <h2 className='text-2xl font-sans antialiased m-3 font-bold text-slate-700'>Pettres</h2>
-          </div>
-          <div className='flex min-w-48 bg-yellow-200'>
-            <div className='flex w-full bg-red-500'></div>
+            <h2 className="text-2xl font-sans antialiased m-3 font-bold text-slate-700">
+              Pettres
+            </h2>
           </div>
         </div>
-        <h1>aa</h1>
+        <div className="flex min-w-64 ">
+          {/* container*/}
+          <div className="flex w-full h-full items-center justify-center">
+            <Avatar>
+              <AvatarImage src="https://static.vecteezy.com/system/resources/previews/009/952/572/non_2x/male-profile-picture-vector.jpg" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col justify-center w-40 m-4 h-full">
+              <h3 className="text-lg font-semibold font-sans antialiased">
+                Nome Sobrenome
+              </h3>
+              <p className="font-sans antialiased font-medium text-gray-400">Função</p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
