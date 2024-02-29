e28e9e21d27d26156a3a34e8262234c6020f6ef7
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-02-29 15:43:19 -0300

    feature: finish basic implementation of firebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,52 @@
-import { getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation';
-import { app, database } from '../fireBaseConfig';
-import { collection, addDoc } from 'firebase/firestore';
+'use client';
 
-export default async function Home() {
-  const session = await getServerSession();
+import { db } from './firebaseConfig';
+import { collection, getDocs, DocumentData } from 'firebase/firestore';
+import React, { useState, useEffect } from 'react';
 
-  // https://www.freecodecamp.org/news/nextjs-firebase-tutorial-build-an-evernote-clone/
+interface Registro {
+  cliente: string;
+  servico: string;
+  valor: number;
+}
 
-  if (!session) {
-    redirect('/login');
-  }
+export default function Home() {
+  const [registroData, setRegistroData] = useState<Registro[]>([]);
+
+  useEffect(() => {
+    const fetchRegistroData = async () => {
+      try {
+        const data = await getDocs(collection(db, 'registro'));
+        const registroArray: Registro[] = [];
+        data.forEach((doc) => {
+          // Certifique-se de adaptar isso à estrutura real do seu documento
+          const { cliente, servico, valor } = doc.data() as DocumentData;
+          registroArray.push({ cliente, servico, valor });
+        });
+        setRegistroData(registroArray);
+      } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+      }
+    };
+
+    fetchRegistroData();
+  }, []); // Executar apenas uma vez ao montar o componente
+
   return (
     <main className="flex flex-grow flex-col items-center justify-center bg-gray-100 h-full w-full">
       <div>
-        <h1 className="text-center">oi</h1>
-        <p>pagina inicial, só pode vela com login</p>
+        <h1 className="text-center">Registro</h1>
+        <p>Registro de Vendas</p>
+
+        {/* Renderizar os dados recuperados */}
+        <ul>
+          {registroData.map((item, index) => (
+            <li key={index}>
+              Cliente: {item.cliente}, Serviço: {item.servico}, Valor:{' '}
+              {item.valor}
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
